0b3ba4e0ca867eba0386f391cd6fa148b4b99fb5
artem-loban <artem.loban@exsportia.com>
2022-12-11 01:30:51 +0200

    asd

--- src/molecules/personal-card.jsx
+++ src/molecules/personal-card.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import db from '../firebase';
 import { Flex, MediaCard, Text } from 'exsportia-components';
+import { collection, getDoc, doc } from "firebase/firestore";
 /////////////////////////////////////////////////////////////////
+
+const person = async () => {
+  try {
+    const response = await getDoc(doc(db, 'persons', '00000000'));
+    const person = response.data();
+    console.log(person);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 export const PersonalCard = ({
   settings,
@@ -9,6 +21,7 @@ export const PersonalCard = ({
   fathersName = 'fathersName',
   image = 'https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-avatar-placeholder-png-image_3416697.jpg',
 }) => {
+  person();
   return (
     <Flex width='150px' height='200px' flexDirection='column' alignItems='center' justifyContent='center' bg='white' borderRadius='10px' boxShadow='0 0 10px 0 rgba(0, 0, 0, 0.1)'>
       <MediaCard
