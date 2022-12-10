a06e5210c212717db3c9f286460b190be94ec07a
artem-loban <artem.loban@exsportia.com>
2022-12-11 01:11:54 +0200

    asd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { PersonalCard } from './molecules/personal-card';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDf36JNNkmO2f3QWwWeNhI6aOF4RUIpUmo",
+  authDomain: "genealogie-1ece8.firebaseapp.com",
+  projectId: "genealogie-1ece8",
+  storageBucket: "genealogie-1ece8.appspot.com",
+  messagingSenderId: "315391574149",
+  appId: "1:315391574149:web:eac6f34daa06322572a9ec",
+  measurementId: "G-FFKJ5WS5Z9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+analytics.logEvent('test');
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-          Learn React
-        </a>
-      </header>
+      <PersonalCard />
     </div>
   );
 }
